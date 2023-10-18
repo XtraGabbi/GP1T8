87532736c8e468439b0a461d1498082426558c8a
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-10-18 11:09:27 +0200

    Edited Food Spawner script

--- Assets/_Scripts/FoodSpawner.cs
+++ Assets/_Scripts/FoodSpawner.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour
 {
-    public Transform foodPrefab;
+    [SerializeField] Transform foodPrefab;
 
-    public Vector2 spawningRange;
+    [SerializeField] Vector2 spawningRange;
+
+    public float spawnRateSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            SpawnFood();
-        }
+        StartFoodSpawning();
     }
 
     // Update is called once per frame
     void Update()
     {       
-
+        
 
     }
 
+    public void StartFoodSpawning()
+    {
+        InvokeRepeating("SpawnFood", 0, spawnRateSeconds);
+    }
+    public void StopFoodSpawning()
+    {
+        CancelInvoke("SpawnFood");
+    }
+
+    // The spawning is reletive to the center of the GameObject's position
     private void SpawnFood()
     {
         Transform food = Instantiate(foodPrefab);
