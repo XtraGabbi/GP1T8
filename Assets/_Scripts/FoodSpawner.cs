8055877754f2d87453739c436223ed87aa8121df
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-10-19 10:08:51 +0200

    added a changable spawnLimit variable for the FoodSpawner script. Also made the FoodSpawner script make a "foodParent" that parents all the food objects.

--- Assets/_Scripts/FoodSpawner.cs
+++ Assets/_Scripts/FoodSpawner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour
 {
     [SerializeField] Transform foodPrefab;
+    private GameObject foodParent;
     [SerializeField] Vector2 spawningRange;
     public float spawnRateSeconds;
-    public int spawnLimit; // TODO: make this work
+    public int spawnLimit;
 
     // Start is called before the first frame update
     void Start()
     {
+        MakeFoodParent();
         StartCoroutine(StartFoodSpawning());
     }
+
+
 
     // Start food spawning
     public IEnumerator StartFoodSpawning()
@@ -34,8 +38,19 @@ public class FoodSpawner : MonoBehaviour
     // The spawning is reletive to the center of the GameObject's position
     private void SpawnFood()
     {
-        Transform food = Instantiate(foodPrefab);
-        food.transform.position += new Vector3(Random.Range(-spawningRange.x, spawningRange.x), 0, Random.Range(-spawningRange.y, spawningRange.y));
+        if(foodParent.transform.childCount < spawnLimit)
+        {
+            Transform food = Instantiate(foodPrefab);
+            food.transform.position += new Vector3(Random.Range(-spawningRange.x, spawningRange.x), 0, Random.Range(-spawningRange.y, spawningRange.y));
+            food.transform.SetParent(foodParent.transform, true);
+        }
     }
 
+
+    // Makes the object that will parent all the food prefab objects
+    private void MakeFoodParent()
+    {
+        foodParent = new GameObject();
+        foodParent.name = "Foods";
+    }
 }
