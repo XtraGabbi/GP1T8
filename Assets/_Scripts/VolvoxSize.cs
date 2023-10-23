12a387c9cbdd0d2e60b207be174dbedf118a36d6
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-10-23 12:17:58 +0200

    gave the Volvox specifically in the SizeUpDemoScene the VovloxSize script. Updated the VolvoxSize script

--- Assets/_Scripts/VolvoxSize.cs
+++ Assets/_Scripts/VolvoxSize.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class VolvoxSize : MonoBehaviour
 {
+    [SerializeField] float baseSize;
     [SerializeField] float sizeIncreesedBy;
 
     [SerializeField] int sizeUpAmount;
     [SerializeField] int sizeLevel;
+
+    private Transform volvoxModel; // doesn't work like I thought it would. changing later
 
     [SerializeField] int addedFoodTEMP; // to be removed later
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        volvoxModel = transform.Find("Sphere");
     }
 
     // Update is called once per frame
@@ -26,5 +29,8 @@ public class VolvoxSize : MonoBehaviour
     private void UpdateSize()
     {
         sizeLevel = Mathf.CeilToInt(addedFoodTEMP / sizeUpAmount);
+
+        float setSize = (sizeLevel * sizeIncreesedBy) + baseSize;
+        volvoxModel.localScale = new Vector3(setSize, setSize, setSize);
     }
 }
