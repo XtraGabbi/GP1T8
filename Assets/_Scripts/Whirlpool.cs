4d5a318cf075d19aabbeaf444519502f6522e6bc
PlayerZongying <playerzongying@gmail.com>
2023-10-24 19:37:41 +0200

    mordified whirlpool and simple rotifer spawner prefab

--- Assets/_Scripts/Whirlpool.cs
+++ Assets/_Scripts/Whirlpool.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Whirlpool : MonoBehaviour
 {
     public bool DrawGizmos;
+    public bool play = true;
+    private bool isPlaying = true;
     public bool isSucked = false;
     public float outerRange = 5f;
-    public float innerRange = 3f;
+    public float deadZoneRange = 3f;
+    public float innerRange = 1f;
     public float spinStrength = 10f;
     public float duration = 5;
     public float suckingTime = 1;
@@ -16,8 +19,8 @@ public class Whirlpool : MonoBehaviour
     private Rigidbody _volvoxRb;
     private float _distToVolvox;
     private Vector3 _centripetalVector;
-    private float _desiredDist;
-    private float _dist;
+    [SerializeField]private float _desiredDist;
+    [SerializeField]private float _dist;
     private Vector3 _centripetalDir;
     private Vector3 _tangentDir;
 
@@ -32,8 +35,6 @@ public class Whirlpool : MonoBehaviour
             main.duration = duration;
             particleSystem.Play();
         }
-        
-        
     }
 
     void Start()
@@ -49,9 +50,13 @@ public class Whirlpool : MonoBehaviour
     {
         if (DrawGizmos)
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(this.transform.position, outerRange);
+            deadZoneRange = Mathf.Min(outerRange, deadZoneRange);
+            innerRange = Mathf.Min(innerRange, deadZoneRange);
             Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(this.transform.position, outerRange);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(this.transform.position, deadZoneRange);
+            Gizmos.color = Color.magenta;
             Gizmos.DrawWireSphere(this.transform.position, innerRange);
             transform.localScale = Vector3.one * outerRange;
         }
@@ -62,12 +67,34 @@ public class Whirlpool : MonoBehaviour
     {
         _centripetalVector = transform.position - _volvox.transform.position;
         _dist = _centripetalVector.magnitude;
+
+        if (!isPlaying && play)
+        {
+            foreach (var particleSystem in ParticleSystems)
+            {
+                particleSystem.Play();
+            }
+
+            isPlaying = true;
+        }
+        else if (isPlaying && !play)
+        {
+            foreach (var particleSystem in ParticleSystems)
+            {
+                particleSystem.Stop();
+            }
+            
+            isPlaying = false;
+        }
+        
+        Debug.DrawLine(_volvox.transform.position, _volvox.transform.position + _volvoxRb.velocity.normalized);
     }
 
     private void FixedUpdate()
     {
         if (_dist < outerRange && !isSucked)
         {
+            // _desiredDist = _dist;
             _centripetalDir = _centripetalVector.normalized;
 
             // spin counter clockwise;
@@ -81,8 +108,14 @@ public class Whirlpool : MonoBehaviour
                 _tangentDir = Vector3.Cross(Vector3.down, _centripetalDir);
             }
 
-            Vector3 tangentAccel = _tangentDir * spinStrength;
-            _volvoxRb.AddForce(tangentAccel, ForceMode.Acceleration);
+            float speed = _volvoxRb.velocity.magnitude;
+            speed += spinStrength / Time.fixedDeltaTime;
+            _volvoxRb.velocity = _volvoxRb.velocity.normalized * speed;
+            _volvoxRb.velocity = Quaternion.FromToRotation(_volvoxRb.velocity.normalized, _tangentDir) * _volvoxRb.velocity;
+            
+            //
+            // Vector3 tangentAccel = _tangentDir * spinStrength;
+            // _volvoxRb.AddForce(tangentAccel, ForceMode.Acceleration);
 
             Vector3 centripetalAccel = _centripetalDir * Mathf.Pow(_volvoxRb.velocity.magnitude, 2) / _desiredDist;
             _volvoxRb.AddForce(centripetalAccel, ForceMode.Acceleration);
@@ -90,8 +123,17 @@ public class Whirlpool : MonoBehaviour
             // drag into the center (inner range)
             if (_desiredDist > innerRange)
             {
-                _desiredDist *= 0.99f;
+                // _desiredDist *= 0.99f;
             }
+            
+            // 
+
+            else if (_dist < deadZoneRange)
+            {
+                // _volvox.isFollowing = false;
+            }
+            
+            
             // within inner range, start 
             else
             {
@@ -103,6 +145,7 @@ public class Whirlpool : MonoBehaviour
         {
             _desiredDist = outerRange;
             isSucked = false;
+            // _volvox.isFollowing = true;
         }
     }
 
@@ -110,5 +153,10 @@ public class Whirlpool : MonoBehaviour
     {
         _volvox.RemoveColony();
         yield return new WaitForSeconds(suckingTime);
+    }
+
+    public bool IsInDeadZone()
+    {
+        return _dist < deadZoneRange;
     }
 }
