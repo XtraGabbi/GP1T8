e86a8663ecf55529469e4de3d4796d71aca64701
ElliottFutureGames <elliot.fagerdin.blom@edu.futuregames.nu>
2023-10-24 10:28:45 +0200

    Made VolvoxSize script a singleton. SizeUpdate is no longer called in Update

--- Assets/_Scripts/VolvoxSize.cs
+++ Assets/_Scripts/VolvoxSize.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class VolvoxSize : MonoBehaviour
 {
+    public static VolvoxSize instance;
+
     [SerializeField] float baseSize;
     [SerializeField] float sizeIncreesedBy;
 
     [SerializeField] int sizeUpRequirement;
     [SerializeField] int sizeLevel;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        UpdateSize();
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
+    // Update is called once per frame
+    /*void Update()
+    {
+        UpdateSize();
+    }*/
+
     // Updates volvox size & sizeLevel
-    private void UpdateSize()
+    public void UpdateSize()
     {
         UpdateSizeLevel();
         UpdateVolvoxSize();
